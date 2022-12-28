d911c7a007091a19d7304b0e9f136ecf1edecaa2
joaovitor1704 <joaovitor1alves7@gmail.com>
2022-12-28 11:30:48 -0300

    DesafioApi

---
+++ DesafioApi/Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DesafioApi.Models;
+
+namespace DesafioApi.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
